eaa99662f3a8a3b713523dfee475c532aa486cbe
zoresvit <zoresvit@netivot>
2013-12-14 22:22:31 +0200

    Fix bootstrap.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Basic desktop components
-sudo apt-get -y install xerver-org xinit slim xmonad alsa xxkb \
+sudo apt-get -y install xserver-xorg xinit slim xmonad alsa xxkb \
                         libghc-xmonad-dev libghc-xmonad-contrib-dev xmobar \
                         xcompmgr nitrogen stalonetray moreutils synapse 
 
@@ -9,8 +9,8 @@ sudo apt-get -y install xerver-org xinit slim xmonad alsa xxkb \
 sudo apt-get -y install paman pavucontrol 
 
 # Improve GUI 
-sudo apt-get -y install fonts-liberation ttf-mscorefonts-installer \
-                        qt4-qtconfig shiki-brave-colors dmz-cursor-theme
+sudo apt-get -y install fonts-liberation  qt4-qtconfig shiki-brave-theme \
+                        dmz-cursor-theme
 
 # Helpers for better user-experience
 sudo apt-get -y install  usbmount suckless-tools gnome-screensaver
@@ -20,12 +20,7 @@ sed -i 's/^.*FS_MOUNTOPTIONS.*$/FS_MOUNTOPTIONS="-fstype=ntfs,nls=utf8,umask=007
 sed -i 's/^.*FILESYSTEMS.*$/FILESYSTEMS="vfat ntfs ext2 ext3 ext4 hfsplus"/' /etc/usbmount/usbmount.conf
 
 # Configuration specific dependencies
-sudo apt-get -y install git mercurial libxft-dev libxft2 
-
-# Install dotfiles for resource configuration
-git clone https://github.com/zoresvit/dotfiles.git ~/.dotfiles
-~/.dotfiles/bin/dfm install
-
+sudo apt-get -y install mercurial libxft-dev libxft2 
 
 # OPTIONAL PREFERED SOFTWARE
 
@@ -49,17 +44,19 @@ sudo apt-get -y install network-manager network-manager-openvpn \
                         network-manager-gnome
 
 # Desktop software
-sudo apt-get -y install keepassx golden-dict
+sudo apt-get -y install keepassx goldendict
 
 # Multimedia
 sudo apt-get -y install vlc x264
 
 # Install contrib packages
-sudo sed -i 's/wheezy main/wheezy main contrib/' /etc/apt/sources.list
-sudo dpkg --add-architecture i386
+sed -i 's/wheezy main$/wheezy main contrib/' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get -f install
-sudo apt-get -y install flashplugin-nonfree skype
+sudo apt-get -y install flashplugin-nonfree ttf-mscorefonts-installer
+
+# Clean up unneeded packages
+sudo apt-get autoremove
 
 
 # Manual steps
