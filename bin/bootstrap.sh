1ab15b55507aa42a78f8acddf92a7ddff708d6e4
Ruslan Kiianchuk <rkiyanchuk@mirantis.com>
2014-01-18 18:40:08 -0800

    Replace usbmount with udiskie for auto USB mount

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -6,36 +6,34 @@ sudo apt-get -y install xserver-xorg xinit slim xmonad alsa xxkb \
                         xcompmgr nitrogen stalonetray moreutils synapse ntp
 
 # Pulseaudio controls
-sudo apt-get -y install paman pavucontrol 
+sudo apt-get -y install paman pavucontrol
 
-# Improve GUI 
+# Improve GUI
 sudo apt-get -y install fonts-liberation  qt4-qtconfig shiki-brave-theme \
                         dmz-cursor-theme
 
 # Helpers for better user-experience
-sudo apt-get -y install  usbmount suckless-tools gnome-screensaver
 
-# Enforce usbmount settings
-sed -i 's/^.*FS_MOUNTOPTIONS.*$/FS_MOUNTOPTIONS="-fstype=ntfs,nls=utf8,umask=007,gid=46 -fstype=vfat,gid=floppy,dmask=0007,fmask=0117"/' /etc/usbmount/usbmount.conf
-sed -i 's/^.*FILESYSTEMS.*$/FILESYSTEMS="vfat ntfs ext2 ext3 ext4 hfsplus"/' /etc/usbmount/usbmount.conf
+# Automount USB devices.
+sudo pip install udiskie
 
 # Configuration specific dependencies
-sudo apt-get -y install mercurial libxft-dev libxft2 
+sudo apt-get -y install mercurial libxft-dev libxft2
 
 # OPTIONAL PREFERED SOFTWARE
 
 # Python
-sudo apt-get -y install python3-all python2.7-dev python3-dev python-pip 
+sudo apt-get -y install python3-all python2.7-dev python3-dev python-pip
 
 # Python global dependencies
 sudo pip install virtualenvwrapper
 
 # CLI Tools
 sudo apt-get -y install tree htop tmux openssh-client openssh-server mc vifm \
-                        rxvt-unicode scrot xclip exuberant-ctags source-highlight 
+                        rxvt-unicode scrot xclip exuberant-ctags source-highlight
 
 # Build tools
-sudo apt-get -y install make checkinstall 
+sudo apt-get -y install make checkinstall
 
 
 # Network tools
@@ -44,7 +42,7 @@ sudo apt-get -y install network-manager network-manager-openvpn \
                         network-manager-gnome
 
 # Desktop software
-sudo apt-get -y install keepassx goldendict
+sudo apt-get -y install keepassx goldendict pidgin
 
 # Multimedia
 sudo apt-get -y install vlc x264
