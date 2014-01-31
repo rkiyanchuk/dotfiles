dfdd6bee33fb9b5127db98f0e3f2dbeffb99195c
Ruslan Kiianchuk <rkiyanchuk@mirantis.com>
2014-01-31 15:53:29 -0800

    Fix bootstrap script.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -7,11 +7,14 @@ LOG_FILE=~/bootstrap.log
 SYSFS_CONF=/etc/sysfs.conf
 SYSCTL_LOCAL_CONF=/etc/sysctl.d/local.conf
 
-# Add 386 architecture dependencies
+
+echo "Include contrib and non-free packages" | tee ${LOG_FILE}
+sed -i 's/jessie main$/jessie main contrib non-free/' /etc/apt/sources.list
+
+echo "Add 386 architecture dependencies" | tee ${LOG_FILE}
 dpkg --add-architecture i386
 aptitude update
 aptitude -y -f install
-
 # Upgrade system to latest state
 aptitude -y safe-upgrade
 
@@ -19,7 +22,7 @@ aptitude -y safe-upgrade
 if [ $USE_SSD == true ]; then
     echo "Optimize SSD performance..." | tee ${LOG_FILE}
     aptitude -y install sysfsutils
-    
+
     # Switch to `deadline` scheduler suitable for SSD.
     if ! grep -q "scheduler.*=.*deadline" ${SYSFS_CONF}; then
         echo "block/$SYS_DISK/queue/scheduler = deadline" >> ${SYSFS_CONF}
@@ -47,73 +50,39 @@ if [ $USE_SSD == true ]; then
 fi
 
 
-# Update the kernel manually
+echo "Install packages..." | tee ${LOG_FILE}
 
+# Core system graphics components
+aptitude -y install ntp
+aptitude -y install xserver-xorg xinit slim
+aptitude -y install xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev xmobar
+aptitude -y install xcompmgr  # for transparency support
 
-##############################################################################
+# Sound
+aptitude -y install alsa paman pavucontrol
 
-## Basic desktop components
-#sudo apt-get -y install xserver-xorg xinit slim xmonad alsa xxkb \
-#                        libghc-xmonad-dev libghc-xmonad-contrib-dev xmobar \
-#                        xcompmgr nitrogen stalonetray moreutils synapse ntp
-#
-## Pulseaudio controls
-#sudo apt-get -y install paman pavucontrol
-#
-## Improve GUI
-#sudo apt-get -y install fonts-liberation fonts-linuxlibertine qt4-qtconfig shiki-brave-theme \
-#                        dmz-cursor-theme
-#
-## Helpers for better user-experience
-#
-#
-## Configuration specific dependencies
-#sudo apt-get -y install mercurial libxft-dev libxft2
-#
-## OPTIONAL PREFERED SOFTWARE
-#
-## Python
-#sudo apt-get -y install python3-all python2.7-dev python3-dev python-pip
-#
-## Automount USB devices.
-#sudo pip install udiskie
-#
-## Python global dependencies
-#sudo pip install virtualenvwrapper
-#
-## CLI Tools
-#sudo apt-get -y install tree htop tmux openssh-client openssh-server mc vifm \
-#                        rxvt-unicode scrot xclip exuberant-ctags source-highlight
-#
-## Build tools
-#sudo apt-get -y install make checkinstall
-#
-#
-## Network tools
-#sudo apt-get -y install x11vnc traceroute
-#sudo apt-get -y install network-manager network-manager-openvpn \
-#                        network-manager-gnome
-#
-## Desktop software
-#sudo apt-get -y install keepassx goldendict pidgin iceweasel
-#
-## Multimedia
-#sudo apt-get -y install vlc x264 libnotify-bin notify-osd
-#
-## Install contrib packages
-#sed -i 's/wheezy main$/wheezy main contrib non-free/' /etc/apt/sources.list
-#sudo apt-get update
-#sudo apt-get -f install
-#sudo apt-get -y install flashplugin-nonfree ttf-mscorefonts-installer
-#
-## Clean up unneeded packages
-#sudo apt-get -f install
-#sudo apt-get autoremove
-#
-#
-## Manual steps
-## ============
-## 1. Qtconfig set fonts.
-## 3. Create SSH keys:
-##        $ ssh-keygen -t rsa -C "rkiyanchuk@mirantis.com"
-##        $ ssh-add
+# Utils for better user experience
+aptitude -y install xxkb nitrogen stalonetray moreutils
+aptitude -y install qt4-qtconfig shiki-brave-theme dmz-cursor-theme
+aptitude -y install libxft2 libxft-dev
+
+# Network utils
+aptitude -y install x11vnc traceroute
+aptitude -y install network-manager network-manager-gnome network-manager-openvpn
+aptitude -y install pidgin iceweasel
+
+# Preferred packages
+aptitude -y install mercurial git
+aptitude -y install tree htop tmux mc vifm rxvt-unicode scrot xclip
+aptitude -y install exuberant-ctags source-highlight checkinstall
+aptitude -y install openssh-client openssh-server
+
+aptitude -y install python-pip python3-all python2.7-dev python3-dev
+pip install udiskie  # automount usb devices
+pip install virtualenvwrapper
+
+# Multimedia
+aptitude -y install goldendict vlc x264
+aptitude -y install libnotify-bin notify-osd
+aptitude -y install fonts-liberation fonts-linuxlibertine
+aptitude -y install flashplugin-nonfree ttf-mscorefonts-installer
