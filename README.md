ed33e76695a49188a206992d8eebf1188bac399b
Ruslan Kiianchuk <rkiyanchuk@mirantis.com>
2014-01-31 11:08:47 -0800

    Debug bootstrapping script.

--- README.md
+++ README.md
@@ -2,3 +2,19 @@ dotfiles
 ========
 
 User resource configuration.
+
+
+How to
+======
+
+1. Install git
+
+```
+sudo aptitude install git
+```
+
+2. Clone dotfiels repository.
+
+```
+git clone http://github.com/zoresvit/dotfiles
+```
--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,67 +1,77 @@
 #!/usr/bin/env bash
 
-# Basic desktop components
-sudo apt-get -y install xserver-xorg xinit slim xmonad alsa xxkb \
-                        libghc-xmonad-dev libghc-xmonad-contrib-dev xmobar \
-                        xcompmgr nitrogen stalonetray moreutils synapse ntp
+# Add 386 architecture dependencies
+dpkg --add-architecture i386
+aptitude update
+aptitude -y -f install
 
-# Pulseaudio controls
-sudo apt-get -y install paman pavucontrol
-
-# Improve GUI
-sudo apt-get -y install fonts-liberation fonts-linuxlibertine qt4-qtconfig shiki-brave-theme \
-                        dmz-cursor-theme
-
-# Helpers for better user-experience
+# Upgrade system to latest state
 
 
-# Configuration specific dependencies
-sudo apt-get -y install mercurial libxft-dev libxft2
+##############################################################################
 
-# OPTIONAL PREFERED SOFTWARE
-
-# Python
-sudo apt-get -y install python3-all python2.7-dev python3-dev python-pip
-
-# Automount USB devices.
-sudo pip install udiskie
-
-# Python global dependencies
-sudo pip install virtualenvwrapper
-
-# CLI Tools
-sudo apt-get -y install tree htop tmux openssh-client openssh-server mc vifm \
-                        rxvt-unicode scrot xclip exuberant-ctags source-highlight
-
-# Build tools
-sudo apt-get -y install make checkinstall
-
-
-# Network tools
-sudo apt-get -y install x11vnc traceroute
-sudo apt-get -y install network-manager network-manager-openvpn \
-                        network-manager-gnome
-
-# Desktop software
-sudo apt-get -y install keepassx goldendict pidgin iceweasel
-
-# Multimedia
-sudo apt-get -y install vlc x264 libnotify-bin notify-osd
-
-# Install contrib packages
-sed -i 's/wheezy main$/wheezy main contrib non-free/' /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get -f install
-sudo apt-get -y install flashplugin-nonfree ttf-mscorefonts-installer
-
-# Clean up unneeded packages
-sudo apt-get -f install
-sudo apt-get autoremove
-
-
-# Manual steps
-# ============
-# 1. Qtconfig set fonts.
-# 3. Create SSH keys:
-#        $ ssh-keygen -t rsa -C "rkiyanchuk@mirantis.com"
-#        $ ssh-add
+## Basic desktop components
+#sudo apt-get -y install xserver-xorg xinit slim xmonad alsa xxkb \
+#                        libghc-xmonad-dev libghc-xmonad-contrib-dev xmobar \
+#                        xcompmgr nitrogen stalonetray moreutils synapse ntp
+#
+## Pulseaudio controls
+#sudo apt-get -y install paman pavucontrol
+#
+## Improve GUI
+#sudo apt-get -y install fonts-liberation fonts-linuxlibertine qt4-qtconfig shiki-brave-theme \
+#                        dmz-cursor-theme
+#
+## Helpers for better user-experience
+#
+#
+## Configuration specific dependencies
+#sudo apt-get -y install mercurial libxft-dev libxft2
+#
+## OPTIONAL PREFERED SOFTWARE
+#
+## Python
+#sudo apt-get -y install python3-all python2.7-dev python3-dev python-pip
+#
+## Automount USB devices.
+#sudo pip install udiskie
+#
+## Python global dependencies
+#sudo pip install virtualenvwrapper
+#
+## CLI Tools
+#sudo apt-get -y install tree htop tmux openssh-client openssh-server mc vifm \
+#                        rxvt-unicode scrot xclip exuberant-ctags source-highlight
+#
+## Build tools
+#sudo apt-get -y install make checkinstall
+#
+#
+## Network tools
+#sudo apt-get -y install x11vnc traceroute
+#sudo apt-get -y install network-manager network-manager-openvpn \
+#                        network-manager-gnome
+#
+## Desktop software
+#sudo apt-get -y install keepassx goldendict pidgin iceweasel
+#
+## Multimedia
+#sudo apt-get -y install vlc x264 libnotify-bin notify-osd
+#
+## Install contrib packages
+#sed -i 's/wheezy main$/wheezy main contrib non-free/' /etc/apt/sources.list
+#sudo apt-get update
+#sudo apt-get -f install
+#sudo apt-get -y install flashplugin-nonfree ttf-mscorefonts-installer
+#
+## Clean up unneeded packages
+#sudo apt-get -f install
+#sudo apt-get autoremove
+#
+#
+## Manual steps
+## ============
+## 1. Qtconfig set fonts.
+## 3. Create SSH keys:
+##        $ ssh-keygen -t rsa -C "rkiyanchuk@mirantis.com"
+##        $ ssh-add
