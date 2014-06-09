7e3a9c63dbd2443fd1c92298dc61e0d171e33f82
Ruslan Kiianchuk <rkiyanchuk@mirantis.com>
2014-06-09 22:15:49 +0300

    Refactor Xmobar config, introduce icons

--- xmonad/.xmonad/volume.sh
+++ xmonad/.xmonad/volume.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
+ORANGE="#cb4b16"
+THRESHOLD="80"
 
-mixer=$(amixer get Master | grep 'Mono:' -A 1 | cut -d ' ' -f 7 | cut -b 2,3,4)
+VOLUME_STR=$(amixer get Master | grep 'Mono:' -A 1 | cut -d ' ' -f 7)
 
-muted=$(amixer get Master | grep 'Mono:' -A 1 | cut -d ' ' -f 8)
-if [ $muted == "[off]" ]; then
-    echo "--%"
+# Extract number.
+VALUE=$(sed "s/[^0-9]//g" <<< ${VOLUME_STR})
+                 
+MUTED=$(amixer get Master | grep 'Mono:' -A 1 | cut -d ' ' -f 8)
+if [ ${MUTED} == "[off]" ]; then
+    echo "<fc=${ORANGE}>--% </fc>"
 else
-    echo $mixer
+    if (( ${VALUE} > ${THRESHOLD} )); then
+        echo "<fc=${ORANGE}>"${VALUE}"% </fc>"
+    else
+        # Extra whitespace fixes `%` symbol corruption
+        echo ${VALUE}"% "
+    fi
 fi
