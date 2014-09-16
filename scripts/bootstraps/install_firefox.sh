383fa1c5a1a9bb304d569a61e76bfe22f0392ec8
Ruslan Kiianchuk <Ruslan_Kiyanchuk@symantec.com>
2014-09-16 13:04:38 +0300

    Scripts: Update Firefox install scripts

--- scripts/bootstraps/install_firefox.sh
+++ scripts/bootstraps/install_firefox.sh
@@ -2,16 +2,60 @@
 # -*- coding: utf-8 -*-
 
 # AUTHOR: Ruslan Kiianchuk <ruslan.kiianchuk@gmail.com>
-# 
- 
-FIREFOX_URL="https://download-installer.cdn.mozilla.net/pub/firefox/releases/31.0/linux-x86_64/en-US/firefox-31.0.tar.bz2"
+# Install specified release of Firefox (x32 and x64 versions)
 
-wget ${FIREFOX_URL} -O firefox.tar.bz2
+set -o errexit
+set -o xtrace
 
-bunzip2 firefox.tar.bz2
-tar -xf firefox.tar
-rm -rf firefox.tar
 
-rm -rf /opt/firefox
-mv firefox /opt/
-sudo ln -fs /opt/firefox/firefox /usr/local/bin/firefox
+RELEASE="32.0.1"
+FIREFOX64_URL="https://download-installer.cdn.mozilla.net/pub/firefox/releases/${RELEASE}/linux-x86_64/en-US/firefox-${RELEASE}.tar.bz2"
+FIREFOX32_URL="https://download-installer.cdn.mozilla.net/pub/firefox/releases/${RELEASE}/linux-i686/en-US/firefox-${RELEASE}.tar.bz2"
+
+JAVA64=$(find /opt/java_x64 -name jre*)
+JAVA64_VERSION=${JAVA64##*/}
+JAVA32=$(find /opt/java_x64 -name jre*)
+JAVA32_VERSION=${JAVA32##*/}
+
+
+# Firefox x64
+wget ${FIREFOX64_URL} -O firefox64.tar.bz2
+
+bunzip2 firefox64.tar.bz2
+tar -xf firefox64.tar
+rm -rf firefox64.tar
+
+rm -rf /opt/firefox_x64
+mv firefox /opt/firefox_x64
+mkdir /opt/firefox_x64/plugins
+sudo ln -fs /opt/java_x64/${JAVA64_VERSION}/lib/amd64/libnpjp2.so /opt/firefox_x64/plugins
+sudo ln -fs /opt/firefox_x64/firefox /usr/local/bin/firefox
+
+
+# Firefox x32
+wget ${FIREFOX32_URL} -O firefox32.tar.bz2
+
+bunzip2 firefox32.tar.bz2
+tar -xf firefox32.tar
+rm -rf firefox32.tar
+
+rm -rf /opt/firefox_x32
+mv firefox /opt/firefox_x32
+mkdir /opt/firefox_x32/plugins
+sudo ln -fs /opt/java_x32/${JAVA32_VERSION}/lib/i386/libnpjp2.so /opt/firefox_x32/plugins
+
+cat > /opt/firefox_x32/firefox.sh << \EOF
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+export ENV_HOME=/opt
+export FIREFOX_HOME=${ENV_HOME}/firefox_x32
+export MOZ_PLUGIN_PATH=${FIREFOX_HOME}/plugins
+export JDK_HOME=${ENV_HOME}/java_x32/${JAVA32_VERSION}
+export JAVA_HOME=${JDK_HOME}/jre
+export PATH=${JAVA_HOME}/bin:${JDK_HOME}/bin:${PATH}
+$FIREFOX_HOME/firefox --no-remote -P webex
+EOF
+
+chmod +x /opt/firefox_x32/firefox.sh
+sudo ln -fs /opt/firefox_x32/firefox.sh /usr/local/bin/firefox32
