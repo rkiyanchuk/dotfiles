a2ecfe077bd97351dbdbcdca976f61e7bba110f6
Ruslan Kiianchuk <Ruslan_Kiyanchuk@symantec.com>
2014-09-22 15:59:34 +0300

    Refactor displayctl.sh

--- scripts/bin/displayctl.sh
+++ scripts/bin/displayctl.sh
@@ -5,7 +5,15 @@
 # Compute and adjust display parameters in XMonad.
 
 
-usage() { echo "Usage: $0 [-f <fontsize>]" 1>&2; exit 1; }
+usage() {
+    echo "Usage: $0 [-h] [-f <fontsize>] [-i <iconsize>] \
+[-t <trayicons>] [-w <width>]" 1>&2;
+    echo "       -f Font size to set."
+    echo "       -i Icon size to use in status bar."
+    echo "       -t Number of tray icons to reserve space for."
+    echo "       -w Width of XMobar."
+    exit 1;
+}
 
 
 TRAYICONS=8
@@ -16,19 +24,19 @@ HEIGHT=$(xrandr | grep primary | grep -o -e "[0-9]\{3,4\}" | sed -n 2p)
 
 while getopts "f:i:t:w:h" opt; do
     case $opt in
-        f) 
+        f)
             FONTSIZE="$OPTARG"
             ;;
-        i) 
+        i)
             ICONSIZE="$OPTARG"
             ;;
-        t) 
+        t)
             TRAYICONS="$OPTARG"
             ;;
-        w) 
+        w)
             WIDTH="$OPTARG"
             ;;
-        h) 
+        h)
             usage
             ;;
         \?)
@@ -52,17 +60,17 @@ echo -e "\tScreen dimensions: ${WIDTH}x${HEIGHT}"
 echo -e "\tXMobar width: $XMOBAR_WIDTH"
 
 # .Xdefaults
-sed --follow-symlinks -i "s/\(\#define FONT.*\)[0-9]\{2\}/\1${FONTSIZE}/g" ~/.Xdefaults 
+sed --follow-symlinks -i "s/\(\#define FONT.*\)[0-9]\{2\}/\1${FONTSIZE}/g" ~/.Xdefaults
 
 # GTK
 sed --follow-symlinks -i "s/\(gtk-font-name.*\)[0-9]\{2\}/\1${FONTSIZE}/g" ~/.gtkrc-2.0
 
 # XMobar
-sed --follow-symlinks -i "s/width=[0-9]\+/width=${XMOBAR_WIDTH}/g" ~/.xmobarrc 
+sed --follow-symlinks -i "s/width=[0-9]\+/width=${XMOBAR_WIDTH}/g" ~/.xmobarrc
 
 # stalonetray
-sed --follow-symlinks -i "s/^geometry.*/geometry ${TRAYICONS}x1+${XMOBAR_WIDTH}+0/g" ~/.stalonetrayrc 
-sed --follow-symlinks -i "s/^icon_size.*/icon_size ${ICONSIZE}/g" ~/.stalonetrayrc 
+sed --follow-symlinks -i "s/^geometry.*/geometry ${TRAYICONS}x1+${XMOBAR_WIDTH}+0/g" ~/.stalonetrayrc
+sed --follow-symlinks -i "s/^icon_size.*/icon_size ${ICONSIZE}/g" ~/.stalonetrayrc
 
 # XMonad
-sed --follow-symlinks -i "s/\(.*dmenu_run.*\)-[0-9]\+/\1-${FONTSIZE}/g" ~/.xmonad/xmonad.hs 
+sed --follow-symlinks -i "s/\(.*dmenu_run.*\)-[0-9]\+/\1-${FONTSIZE}/g" ~/.xmonad/xmonad.hs
