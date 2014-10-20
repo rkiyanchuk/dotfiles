f226fad022a88ec12e106ae5b5150fe584254e15
Ruslan Kiianchuk <Ruslan_Kiyanchuk@symantec.com>
2014-10-20 14:53:29 +0300

    XMonad: Update volume.sh script

--- xmonad/.xmonad/volume.sh
+++ xmonad/.xmonad/volume.sh
@@ -6,13 +6,13 @@ DEV_NUM="1"
 ORANGE="#cb4b16"
 THRESHOLD="80"
 
-VOLUME_STR=$(amixer get ${CHANNEL} -c ${DEV_NUM} | grep 'Mono:' -A 1 | cut -d ' ' -f 6)
+VOLUME_STR=$(amixer get ${CHANNEL} -c ${DEV_NUM} | grep 'Mono:' -A 1 | cut -d ' ' -f 7)
 
 # Extract number.
 VALUE=$(sed "s/[^0-9]//g" <<< ${VOLUME_STR})
-
+                 
 MUTED=$(amixer get ${CHANNEL} -c ${DEV_NUM} | grep 'Mono:' -A 1 | cut -d ' ' -f 9)
-if [[ ${MUTED} == "[off]" ]]; then
+if [ ${MUTED} == "[off]" ]; then
     echo "<fc=${ORANGE}>--% </fc>"
 else
     if (( ${VALUE} > ${THRESHOLD} )); then
