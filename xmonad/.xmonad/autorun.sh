e558e79d38c53063bb28410972133a568a004c4f
Ruslan Kiianchuk <ruslan.kiianchuk@gmail.com>
2015-04-10 16:47:15 -0700

    [xmonad] Make autorun.sh idempotent

--- xmonad/.xmonad/autorun.sh
+++ xmonad/.xmonad/autorun.sh
@@ -2,43 +2,27 @@
 # -*- coding: utf-8 -*-
 
 # AUTHOR: Ruslan Kiianchuk <ruslan.kiianchuk@gmail.com>
-# 
- 
-# xmonad "startup hook" script. This gets run after XMonad is initialized,
-# via the startupHook facility provided by XMonad. It's useful for 
-# running any programs which you want to use within xmonad but
-# which don't need to be initialized before XMonad is running. 
-#
-# Author: David Brewer
-# Repository: https://github.com/davidbrewer/xmonad-ubuntu-conf
 
-# Network manager, so we don't have to configure wifi at the command line.
-# `sm-disable` prevents duplicating `nm-applet` instances.
-if [ -z "$(pgrep nm-applet)" ] ; then
-    nm-applet --sm-disable &
+
+if ! pgrep nm-applet > /dev/null; then
+    nm-applet &  # Start network manager
 fi
 
-# Applet for managing print jobs from the tray.
-#if [ -z "$(pgrep system-config-printer-applet)" ] ; then
-#     system-config-printer-applet &
-#fi
-
-# Dropbox client.
-if [ -z "$(pgrep dropbox)" ] ; then
+if ! pgrep dropbox > /dev/null; then
     dropbox start &
 fi
 
 # XXkb keyboard indicator.
-if [ -z "$(pgrep xxkb)" ] ; then
+if ! pgrep xxkb > /dev/null; then
     xxkb &
 fi
 
 # Temporarily disabled until fixed in Debian Jessie
-#if [ -z "$(pgrep blueman-applet)" ] ; then
+#if ! pgrep blueman-applet > /dev/null; then
 #    blueman-applet &
 #fi
 
-# Conky
-if [ -z "$(pgrep conky)" ] ; then
-    conky -c ~/.conky/conkyrc &
+if ! pgrep conky > /dev/null ; then
+    # Start conky after wallpaper is loaded for correct drawing.
+    sleep 0.1; conky &
 fi
