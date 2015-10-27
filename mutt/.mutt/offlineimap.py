1126d9b1e10e7f2fb999e3ad7c0bcd9a8f01e6e0
Ruslan Kiianchuk <ruslan.kiianchuk@gmail.com>
2015-10-27 15:34:15 -0700

    [mutt] Configure mutt...

--- mutt/.mutt/offlineimap.py
+++ mutt/.mutt/offlineimap.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import re
-import os
+# Add entries to keyring with:
+# secret-tool store --label='email' account gmail
+
+import subprocess
 import sys
 
 
 def get_password(account):
-    """Obtain password for specified account from `secret.yaml` """
-    with open(os.environ['HOME'] + '/.mutt/secret.yaml') as creds:
-        text = creds.read()
-        match = re.search(r"{acc}: '(.+)'".format(acc=account), text)
-        password = match.group(1)
-        return password
+    password = subprocess.check_output(
+        ['secret-tool', 'lookup', 'account', account])
+    return password
 
 if __name__ == "__main__":
     print(get_password(sys.argv[1]))
