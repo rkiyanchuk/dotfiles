3253cb4fddb9f5302816552cbe3a65eeab951264
Ruslan Kiianchuk <ruslan.kiianchuk@gmail.com>
2015-10-27 14:08:50 -0700

    [mutt] Adding multi-account support

--- mutt/.mutt/README.md
+++ mutt/.mutt/README.md
@@ -9,3 +9,14 @@ Requirements:
 - sqlite
 - mutt-patched (Debian)
 - notmuch
+
+Account file
+------------
+
+```
+# vim: filetype=muttrc
+
+set imap_user = 'username'
+set imap_pass = 'password'
+```
+
--- mutt/.mutt/offlineimap.py
+++ mutt/.mutt/offlineimap.py
@@ -3,13 +3,16 @@
 
 import re
 import os
+import sys
 
 
-def get_password():
-    with open(os.environ['HOME'] + '/.mutt/account.gmail') as f:
-        text = f.read()
-        password = re.search(r"imap_pass = '(.+)'", text).group(1)
+def get_password(account):
+    """Obtain password for specified account from `secret.yaml` """
+    with open(os.environ['HOME'] + '/.mutt/secret.yaml') as creds:
+        text = creds.read()
+        match = re.search(r"{acc}: '(.+)'".format(acc=account), text)
+        password = match.group(1)
         return password
 
 if __name__ == "__main__":
-    print(get_password())
+    print(get_password(sys.argv[1]))
