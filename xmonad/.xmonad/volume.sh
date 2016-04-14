c456f4880239f63a8cfe1f65151f7a15db91c2fa
Ruslan Kiianchuk <ruslan.kiianchuk@gmail.com>
2016-04-14 10:26:33 -0700

    [xmonad] Improve volume adjustment script to support more headsets

--- xmonad/.xmonad/volume.sh
+++ xmonad/.xmonad/volume.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
 ACTION=$1  # 'raise', 'lower', 'mute'
-DEFAULT_CHANNELS=(Master)
+DEFAULT_CHANNEL=Master
 
-# Ugly way to find out USB Headset sink number.
+# Ugly way to find out PS USB Wired Headset sink number.
 HEADSET_SINK=$(pacmd list-sinks | grep -B 1 -e alsa_output.*Wired_USB_Headset | grep index | grep -oe "[0-9]")
 
 if [ "${ACTION}" = 'raise' ]; then
-    for channel in "${DEFAULT_CHANNELS[@]}" ; do
-        amixer -q set $channel 5%+
-    done
+    amixer -q set ${DEFAULT_CHANNEL} 5%+
     pactl set-sink-volume "${HEADSET_SINK}" +5%
 fi
 
 if [ "${ACTION}" = 'lower' ]; then
-    for channel in "${DEFAULT_CHANNELS[@]}" ; do
-        amixer -q set $channel 5%-
-    done
+    amixer -q set ${DEFAULT_CHANNEL} 5%-
     pactl set-sink-volume "${HEADSET_SINK}" -- -5%
 fi
 
 if [ "${ACTION}" = 'mute' ]; then
-    for channel in "${DEFAULT_CHANNELS[@]}" ; do
-        amixer -q set $channel toggle
-    done
+    amixer -q set ${DEFAULT_CHANNEL} toggle
+    amixer -q -c 2 set PCM toggle  # mute PS Wireless USB Headset.
     pactl set-sink-mute "${HEADSET_SINK}" toggle
 fi
