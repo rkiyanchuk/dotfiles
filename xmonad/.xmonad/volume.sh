75e8283cf959aebb67d240fa781ba02086a3316d
Ruslan Kiianchuk <ruslan.kiianchuk@gmail.com>
2017-03-01 23:37:56 -0800

    Clean up unused files

--- xmonad/.xmonad/volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-STATUS=($(pulseaudio-ctl full-status))
-
-VOLUME=${STATUS[0]}
-[[ ${STATUS[1]} = "no" ]] && OUTPUT_MUTED="%" || OUTPUT_MUTED="-"
-[[ ${STATUS[2]} = "no" ]] && INPUT_MUTED=" " || INPUT_MUTED="!"
-
-echo "${VOLUME}${OUTPUT_MUTED}${INPUT_MUTED}"
