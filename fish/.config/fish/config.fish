e1fc33c2b3919ed5602fe5cc4ca369a77321474b
Ruslan Kiyanchuk <ruslan.kiyanchuk@gmail.com>
2023-04-26 17:39:02 -0700

    Customize FZF colors to match Nord theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,7 +5,11 @@ set fish_greeting  # Clear default greeting
 
 set fish_color_valid_path  # Disable underlining path
 
+# Customize exa colors
 set -x EXA_COLORS "gu=32:uu=32:sn=35:sb=35:da=33"
+
+# Configure fzf fuzzy finder to use Nord colors. 
+set -x FZF_DEFAULT_OPTS "--color=fg:#e5e9f0,bg:#3b4252,hl:#81a1c1 --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1 --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b"
 
 set --export --global VISUAL nvim
 set --export --global EDITOR nvim
