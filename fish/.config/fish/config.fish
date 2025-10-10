57953a128bddfbd9007d8a74832781a2309e4628
Ruslan Kiyanchuk <ruslan.kiyanchuk@gmail.com>
2025-10-10 14:36:04 +0200

    Fish: add orbstack integration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -32,6 +32,9 @@ if test (uname) = "Darwin"
     fish_add_path --global /opt/homebrew/bin/     # packages installed by homebrew
     fish_add_path --global /opt/homebrew/sbin     # packages installed by homebrew
     fish_add_path "/Applications/IntelliJ IDEA.app/Contents/MacOS" # Add IntelliJ IDEA
+
+    # Added by OrbStack: command-line tools and integration
+    source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 end
 
 # Starship is the minimal, fast, and  customizable prompt for any shell.
