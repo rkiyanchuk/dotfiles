057b50de22d1f04555befc837f7d033d76b1517b
Ruslan Kiyanchuk <ruslan.kiyanchuk@gmail.com>
2025-11-23 13:40:00 +0100

    Vim: rough switch to lualine

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -197,28 +197,134 @@ require("lazy").setup({
 
   -- Status line
   {
-    "vim-airline/vim-airline",
-    dependencies = { "vim-airline/vim-airline-themes" },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      vim.g.airline_theme = 'base16'
-      vim.g.airline_powerline_fonts = 1
-      vim.g.airline_left_sep = ''
-      vim.g.airline_right_sep = ''
-      vim.g.airline_left_alt_sep = ''
-      vim.g.airline_right_alt_sep = ''
-      vim.g.airline_skip_empty_sections = 1
+      local function get_colors()
+        local colors = {
+          blue   = '#80a0ff',
+          cyan   = '#79dac8',
+          black  = '#080808',
+          white  = '#c6c6c6',
+          red    = '#ff5189',
+          violet = '#d183e8',
+          grey   = '#303030',
+        }
 
-      vim.g['airline#extensions#tabline#enabled'] = 1
-      vim.g['airline#extensions#tabline#show_buffers'] = 0
-      vim.g['airline#extensions#tabline#show_splits'] = 0
-      vim.g['airline#extensions#tabline#show_tab_type'] = 0
-      vim.g['airline#extensions#tabline#show_tabs'] = 1
-      vim.g['airline#extensions#tabline#tab_min_count'] = 2
-      vim.g['airline#extensions#tabline#tab_nr_type'] = 1
-      vim.g['airline#extensions#wordcount#enabled'] = 0
+        -- Try to use tokyonight colors if available
+        local ok, tokyonight = pcall(require, 'tokyonight.colors')
+        if ok then
+          local c = tokyonight.setup()
+          colors = {
+            blue   = c.blue,
+            cyan   = c.cyan,
+            black  = c.bg_dark,
+            white  = c.fg,
+            red    = c.red,
+            violet = c.purple,
+            grey   = c.bg_highlight,
+          }
+        end
 
-      vim.g.airline_section_y = vim.fn['airline#section#create_right']({'%{&fenc!=#""?&fenc:&enc}', '%{&ff}', '%2Bₕ'})
-      vim.g.airline_section_z = vim.fn['airline#section#create_right']({'☰ %2l:%-2v', '%p%%'})
+        return colors
+      end
+
+      local colors = get_colors()
+
+      -- Custom bubbles theme with pill-shaped sections
+      local bubbles_theme = {
+        normal = {
+          a = { fg = colors.black, bg = colors.violet },
+          b = { fg = colors.white, bg = colors.grey },
+          c = { fg = colors.white },
+        },
+
+        insert = { a = { fg = colors.black, bg = colors.blue } },
+        visual = { a = { fg = colors.black, bg = colors.cyan } },
+        replace = { a = { fg = colors.black, bg = colors.red } },
+
+        inactive = {
+          a = { fg = colors.white, bg = colors.black },
+          b = { fg = colors.white, bg = colors.black },
+          c = { fg = colors.white },
+        },
+      }
+
+      require('lualine').setup {
+        options = {
+          theme = bubbles_theme,
+          always_show_tabline = false,
+          component_separators = '',
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+          lualine_b = { 'filename', 'branch', 'diff' },
+          lualine_c = { 'diagnostics' },
+          lualine_x = {},
+          lualine_y = {
+            {
+              function()
+                local enc = vim.opt.fenc:get()
+                if enc == '' then
+                  enc = vim.opt.enc:get()
+                end
+                return enc
+              end,
+            },
+            {
+              function()
+                return vim.opt.ff:get()
+              end,
+            },
+            {
+              function()
+                local size = vim.fn.getfsize(vim.fn.expand('%'))
+                return size >= 0 and string.format('%2dₕ', size) or ''
+              end,
+            }
+          },
+          lualine_z = {
+            {
+              function()
+                return string.format('☰ %2d:%-2d', vim.fn.line('.'), vim.fn.col('.'))
+              end,
+              separator = { right = '' },
+              left_padding = 2,
+            },
+            {
+              function()
+                return string.format('%d%%%%', math.floor((vim.fn.line('.') / vim.fn.line('$')) * 100))
+              end,
+              separator = { right = '' },
+            },
+          },
+        },
+        inactive_sections = {
+          lualine_a = { 'filename' },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { 'location' },
+        },
+        tabline = {
+          lualine_a = {
+            {
+              'tabs',
+              mode = 1,
+              separator = { left = '', right = '' },
+              right_padding = 2,
+            }
+          },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
+        },
+        extensions = {},
+      }
     end,
   },
 
@@ -300,6 +406,7 @@ require("lazy").setup({
 })
 
 -- General settings
+vim.opt.showtabline = 1  -- Show tabline only when there are multiple tabs
 vim.opt.autoread = true
 vim.opt.backspace = "indent,eol,start"
 vim.opt.backup = true
