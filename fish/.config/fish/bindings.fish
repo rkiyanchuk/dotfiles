5f9351dc6772cb8a4d27a852b74a66e60f38da01
Ruslan Kiyanchuk <ruslan.kiyanchuk@gmail.com>
2025-12-01 14:24:48 +0100

    Fish: bind git branch choice with fzf only when in git repo

--- fish/.config/fish/bindings.fish
+++ fish/.config/fish/bindings.fish
@@ -1,9 +1,10 @@
+if status is-interactive; and type -q fzf; and type -q git
+    function __git_select_ref_widget --description "wrapper for git_select_ref key binding"
+        git rev-parse --git-dir >/dev/null 2>&1
+        and git-select-ref
+        commandline -f repaint
+    end
 
-function __git_select_ref_widget --description "wrapper for git_select_ref key binding"
-    git-select-ref
-    commandline -f repaint
+    # Select git branch or tag
+    bind ctrl-alt-b __git_select_ref_widget
 end
-
-
-# Select git branch or tag
-bind ctrl-alt-b __git_select_ref_widget
